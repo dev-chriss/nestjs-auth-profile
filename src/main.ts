20ae18b3517818a34a090c0b06755ea9c7bfbf98
devchriss <christsetyawan82@gmail.com>
2023-09-25 12:24:50 +0700

    update swagger url

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/swagger', app, document);
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
