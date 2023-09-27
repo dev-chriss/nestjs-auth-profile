2cf09661f82ea4a7deec0fd21bb5f634b5d96a63
devchriss <christsetyawan82@gmail.com>
2023-09-27 13:36:13 +0700

    add simple unit test Profile Controller

--- src/profiles/profiles.controller.spec.ts
+++ src/profiles/profiles.controller.spec.ts
@@ -2,25 +2,141 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ProfilesController } from './profiles.controller';
 import { ProfilesService } from './profiles.service';
 import { getModelToken } from '@nestjs/mongoose';
-import { Profile } from '../schemas/profile.schema';
+import { Profile, ProfileSchema } from '../schemas/profile.schema';
+import { CreateProfileDto } from './dto/create-profile.dto';
+import { UpdateProfileDto } from './dto/update-profile.dto';
+
+// using fake database in RAM for testing
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { Connection, connect, Model, Types } from 'mongoose';
+
+const createProfileDtoStub: CreateProfileDto = {
+  userId: '6512f3ebc4ef8523da20e7ef',
+  displayName: 'User Satu',
+  gender: 'Male',
+  height: 160,
+  weight: 50,
+  birthday: '1995/6/28',
+  zodiac: 'Cancer',
+  horoscope: '',
+  interests: [],
+};
 
 describe('ProfilesController', () => {
-  let controller: ProfilesController;
+  let profileController: ProfilesController;
+  let mongod: MongoMemoryServer;
+  let mongoConnection: Connection;
+  let profileModel: Model<Profile>;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
+    mongod = await MongoMemoryServer.create();
+    const uri = mongod.getUri();
+    mongoConnection = (await connect(uri)).connection;
+    profileModel = mongoConnection.model(Profile.name, ProfileSchema);
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProfilesController],
-      // providers: [ProfilesService],
       providers: [
         ProfilesService,
-        { provide: getModelToken(Profile.name), useValue: jest.fn() },
+        { provide: getModelToken(Profile.name), useValue: profileModel },
       ],
     }).compile();
+    profileController = module.get<ProfilesController>(ProfilesController);
+  });
 
-    controller = module.get<ProfilesController>(ProfilesController);
+  afterAll(async () => {
+    await mongoConnection.dropDatabase();
+    await mongoConnection.close();
+    await mongod.stop();
+  });
+
+  afterEach(async () => {
+    const collections = mongoConnection.collections;
+    for (const key in collections) {
+      const collection = collections[key];
+      await collection.deleteMany({});
+    }
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(profileController).toBeDefined();
+  });
+
+  describe('getProfile', () => {
+    it('should return the corresponding saved object', async () => {
+      await new profileModel(createProfileDtoStub).save();
+      const profile = await profileController.findOne(
+        createProfileDtoStub.userId,
+      );
+      expect(profile.userId).toEqual(
+        new Types.ObjectId(createProfileDtoStub.userId),
+      );
+    });
+
+    it('should return User not found', async () => {
+      try {
+        await profileController.findOne(createProfileDtoStub.userId);
+      } catch (e) {
+        expect(e.message).toBe('User not found');
+      }
+    });
+  });
+
+  describe('createProfile', () => {
+    it('should return the new created object', async () => {
+      const createdProfile = await profileController.create(
+        createProfileDtoStub,
+      );
+      expect(createdProfile.userId).toEqual(
+        new Types.ObjectId(createProfileDtoStub.userId),
+      );
+    });
+
+    it('should return duplicate key (Bad Request - 400) exception', async () => {
+      try {
+        await new profileModel(createProfileDtoStub).save();
+      } catch (e) {
+        expect(e.message).toContain('duplicate key error');
+      }
+    });
+  });
+
+  describe('updateProfile', () => {
+    const updateProfileDtoStub: UpdateProfileDto = {
+      displayName: 'User Satu',
+      gender: 'Male',
+      height: 160,
+      weight: 50,
+      birthday: '1995/6/28',
+      zodiac: 'Cancer',
+      horoscope: '',
+      interests: [],
+    };
+
+    it('should return the new updated object', async () => {
+      await profileController.create(createProfileDtoStub);
+      const updatedProfile = await profileController.update(
+        createProfileDtoStub.userId,
+        updateProfileDtoStub,
+      );
+      expect(updatedProfile.userId).toEqual(
+        new Types.ObjectId(createProfileDtoStub.userId),
+      );
+      expect(updatedProfile.displayName).toEqual(
+        updateProfileDtoStub.displayName,
+      );
+    });
+
+    it('should return duplicate key (Bad Request - 400) exception', async () => {
+      try {
+        await new profileModel(createProfileDtoStub).save();
+        await profileController.update(
+          createProfileDtoStub.userId,
+          updateProfileDtoStub,
+        );
+      } catch (e) {
+        expect(e.message).toContain('duplicate key error');
+      }
+    });
   });
 });
--- src/profiles/profiles.service.spec.ts
+++ src/profiles/profiles.service.spec.ts
@@ -1,137 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProfilesService } from './profiles.service';
+import { getModelToken } from '@nestjs/mongoose';
 import { Profile } from '../schemas/profile.schema';
-import mongoose from 'mongoose';
-import { UpdateProfileDto } from './dto/update-profile.dto';
-import { CreateProfileDto } from './dto/create-profile.dto';
-
-const mockProfile = (id: string): Profile => {
-  return {
-    userId: new mongoose.Schema.Types.ObjectId(id),
-    displayName: 'User Satu',
-    gender: 'Male',
-    height: 160,
-    weight: 50,
-    birthday: '1995/6/28',
-    zodiac: 'Cancer',
-    horoscope: '',
-    interests: [],
-  };
-};
-
-class serviceMock {
-  findByUserId = (id: string): Profile => {
-    return mockProfile(id);
-  };
-
-  updateByUserId = (
-    id: string,
-    updateProfileDto: UpdateProfileDto,
-  ): Profile => {
-    return updateProfileDto as Profile;
-  };
-
-  create = (createProfileDto: CreateProfileDto): Profile => {
-    const newData: Profile = {
-      userId: new mongoose.Schema.Types.ObjectId(createProfileDto.userId),
-      displayName: createProfileDto.displayName,
-      gender: createProfileDto.gender,
-      height: createProfileDto.height,
-      weight: createProfileDto.weight,
-      birthday: createProfileDto.birthday,
-      zodiac: createProfileDto.zodiac,
-      horoscope: createProfileDto.horoscope,
-      interests: createProfileDto.interests,
-    };
-    return newData;
-  };
-}
 
 describe('ProfilesService', () => {
-  let profileService: ProfilesService;
+  let service: ProfilesService;
 
-  beforeAll(async () => {
-    const serviceProvider = {
-      provide: ProfilesService,
-      useClass: serviceMock,
-    };
-
+  beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProfilesService, serviceProvider],
+      providers: [
+        ProfilesService,
+        { provide: getModelToken(Profile.name), useValue: jest.fn() },
+      ],
     }).compile();
 
-    profileService = module.get<ProfilesService>(ProfilesService);
+    service = module.get<ProfilesService>(ProfilesService);
   });
 
   it('should be defined', () => {
-    expect(profileService).toBeDefined();
-  });
-
-  it('should foundOne by ID', async () => {
-    const userId = '6512f3ebc4ef8523da20e7ef';
-    const expectedResult: Profile = {
-      userId: new mongoose.Schema.Types.ObjectId(userId),
-      displayName: 'User Satu',
-      gender: 'Male',
-      height: 160,
-      weight: 50,
-      birthday: '1995/6/28',
-      zodiac: 'Cancer',
-      horoscope: '',
-      interests: [],
-    };
-    const received = await profileService.findByUserId(userId as string);
-    expect(received).toEqual(expectedResult);
-  });
-
-  it('should updateOne by ID', async () => {
-    const userId = '6512f3ebc4ef8523da20e7ef';
-    const expectedResult: Profile = {
-      userId: new mongoose.Schema.Types.ObjectId(userId),
-      displayName: 'User Satu',
-      gender: 'Male',
-      height: 160,
-      weight: 50,
-      birthday: '1995/6/28',
-      zodiac: 'Cancer',
-      horoscope: '',
-      interests: [],
-    };
-    const received = await profileService.updateByUserId(
-      userId as string,
-      expectedResult as UpdateProfileDto,
-    );
-    expect(received).toEqual(expectedResult);
-  });
-
-  it('should create new Profile', async () => {
-    const userId = '6512f3ebc4ef8523da20e7ef';
-
-    const input: CreateProfileDto = {
-      userId,
-      displayName: 'User Satu',
-      gender: 'Male',
-      height: 160,
-      weight: 50,
-      birthday: '1995/6/28',
-      zodiac: 'Cancer',
-      horoscope: '',
-      interests: [],
-    };
-
-    const expectedResult: Profile = {
-      userId: new mongoose.Schema.Types.ObjectId(userId),
-      displayName: 'User Satu',
-      gender: 'Male',
-      height: 160,
-      weight: 50,
-      birthday: '1995/6/28',
-      zodiac: 'Cancer',
-      horoscope: '',
-      interests: [],
-    };
-
-    const received = await profileService.create(input);
-    expect(received).toEqual(expectedResult);
+    expect(service).toBeDefined();
   });
 });
